b9d0240a59798f19b0c6889858b5a59040193f5e
Cooper-James <cooper.jbond@gmail.com>
2024-04-06 08:41:36 -0500

    Navmesh test
    Implemented navmesh test and a simple click to move thingy

--- Assets/CustomPrefabs/Enemy/Enemy.cs
+++ Assets/CustomPrefabs/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Enemy : MonoBehaviour
     public float speed = 5f;
     public int moneyOnKill = 10;
     public int healthLostOnExit = 1;
+    public NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(movePosition, out var hitInfo))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
     }
 }
